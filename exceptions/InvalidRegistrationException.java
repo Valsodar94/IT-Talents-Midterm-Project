4ecc74548e81328ffbaa48ad386d0b7a468f529b
Dzhanan Salim <cani_1991@abv.bg>
2018-03-25 12:44:22 +0300

    Deleted unnecessary package folders from main folder.

--- exceptions/InvalidRegistrationException.java
+++
@@ -1,8 +0,0 @@
-package exceptions;
-
-public class InvalidRegistrationException extends Exception {
-	private static final long serialVersionUID = 7967449227974797079L;
-	public InvalidRegistrationException(String str) {
-		super(str);
-	}
-}
